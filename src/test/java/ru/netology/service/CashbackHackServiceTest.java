a713a5aee4c1c8d4506c06ab477c925436a3f554
Roman Gudov <gudov.p2@mail.ru>
2024-01-19 19:51:43 +0300

    Reformat

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,21 +5,21 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.junit.Test
-    public void notRecommendBuying(){
-        CashbackHackService service=new CashbackHackService();
-        int amount=1000;
-        int actual=service.remain(amount);
-        int expected=0;
-        assertEquals(expected,actual);
+    public void notRecommendBuying() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
     @org.junit.Test
-    public void advisesToBuy(){
-        CashbackHackService service=new CashbackHackService();
-        int amount=1200;
-        int actual=service.remain(amount);
-        int expected=800;
-        assertEquals(expected,actual);
+    public void advisesToBuy() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(expected, actual);
     }
 
 }
